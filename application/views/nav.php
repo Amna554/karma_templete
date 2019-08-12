e65bbb4368a7e0d6daf55d1145e8e0a1aeb4c0b7
Amna <zahidamna05@gmail.com>
2019-08-11 22:28:11 -0700

    working

--- application/views/nav.php
+++ application/views/nav.php
@@ -1,12 +1,8 @@
-
-<!DOCTYPE html>
-<html lang="zxx" class="no-js">
-
-<head>
+<html lang="zxx" class="no-js"><head>
 	<!-- Mobile Specific Meta -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- Favicon-->
-	<link rel="shortcut icon" href="img/fav.png">
+	<link rel="shortcut icon" href="<?php echo base_url();?>karma/img/fav.png">
 	<!-- Author Meta -->
 	<meta name="author" content="CodePixar">
 	<!-- Meta Description -->
@@ -27,24 +23,21 @@
 	<link rel="stylesheet" href="<?php echo base_url();?>karma/css/owl.carousel.css">
 	<link rel="stylesheet" href="<?php echo base_url();?>karma/css/nice-select.css">
 	<link rel="stylesheet" href="<?php echo base_url();?>karma/css/nouislider.min.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>karma/css/ion.rangeSlider.css" />
-	<link rel="stylesheet" href="<?php echo base_url();?>karma/css/ion.rangeSlider.skinFlat.css" />
+	<link rel="stylesheet" href="<?php echo base_url();?>karma/css/ion.rangeSlider.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>karma/css/ion.rangeSlider.skinFlat.css">
 	<link rel="stylesheet" href="<?php echo base_url();?>karma/css/magnific-popup.css">
 	<link rel="stylesheet" href="<?php echo base_url();?>karma/css/main.css">
-</head>
-
+<script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/<?php echo base_url();?>karma/js/37/10a/common.js"></script><script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/<?php echo base_url();?>karma/js/37/10a/util.js"></script><script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps/api/<?php echo base_url();?>karma/js/AuthenticationService.Authenticate?1sfile%3A%2F%2F%2FC%3A%2Fxampp%2Fhtdocs%2Fci6%2Fkarma%2Findex.html&amp;4sAIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE&amp;callback=_xdc_._uup3h&amp;key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE&amp;token=45498"></script></head>
 <body>
-
-	<!-- Start Header Area -->
+     <!-- Start Header Area -->
 	
-	<header class="header_area sticky-header">
-		<div class="main_menu"><br>
+		<header class="header_area sticky-header">
+		<div class="main_menu">
 			<nav class="navbar navbar-expand-lg navbar-light main_box">
 				<div class="container">
 					<!-- Brand and toggle get grouped for better mobile display -->
 					<a class="navbar-brand logo_h" href="index.html"><img src="<?php echo base_url();?>karma/img/logo.png" alt=""></a>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-					 aria-controls="navbarSupportedContent" aria-expanded="" aria-label="Toggle navigation">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
@@ -52,39 +45,36 @@
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
 						<ul class="nav navbar-nav menu_nav ml-auto">
-							<li class="nav-item active"><a class="nav-link" href="<?php echo site_url('Welcome/home')?>">Home</a></li>
+							<li class="nav-item active"><a class="nav-link" href="<?php echo site_url('Welcome/');?>">Home</a></li>
 							<li class="nav-item submenu dropdown">
-								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-								 aria-expanded="false">Shop</a>
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Shop</a>
 								<ul class="dropdown-menu">
-									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/category')?>">Shop Category</a></li>
-									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/product_detail')?>">Product Details</a></li>
-									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/checkout')?>">Product Checkout</a></li>
-									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/shopping_cart')?>">Shopping Cart</a></li>
-									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/confirmation')?>">Confirmation</a></li>
+									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/category');?>">Shop Category</a></li>
+									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/product_detail');?>">Product Details</a></li>
+									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/checkout');?>">Product Checkout</a></li>
+									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/shopping_cart');?>">Shopping Cart</a></li>
+									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/confirmation');?>">Confirmation</a></li>
 								</ul>
 							</li>
 							<li class="nav-item submenu dropdown">
-								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-								 aria-expanded="false">Blog</a>
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
 								<ul class="dropdown-menu">
-									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/blog')?>">Blog</a></li>
-									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/blog_details')?>">Blog Details</a></li>
+									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/blog');?>">Blog</a></li>
+									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/blog_details');?>">Blog Details</a></li>
 								</ul>
 							</li>
 							<li class="nav-item submenu dropdown">
-								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-								 aria-expanded="false">Pages</a>
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
 								<ul class="dropdown-menu">
 									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/login');?>">Login</a></li>
-									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/order_tracking');?></a></li>
+									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/order_tracking');?>">Tracking</a></li>
 									<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/elements');?>">Elements</a></li>
 								</ul>
 							</li>
-							<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/contact');?>">Contact</a></li>
+							<li class="nav-item"><a class="nav-link" href="<?php echo site_url('Welcome/');?>">Contact</a></li>
 						</ul>
 						<ul class="nav navbar-nav navbar-right">
-							<li class="nav-item"><a href="" class="cart"><span class="ti-bag"></span></a></li>
+							<li class="nav-item"><a href="#" class="cart"><span class="ti-bag"></span></a></li>
 							<li class="nav-item">
 								<button class="search"><span class="lnr lnr-magnifier" id="search"></span></button>
 							</li>
@@ -94,4 +84,16 @@
 			</nav>
 		</div>
 		
+		<div class="search_input" id="search_input_box" style="display: none;">
+			<div class="container">
+				<form class="d-flex justify-content-between">
+					<input type="text" class="form-control" id="search_input" placeholder="Search Here">
+					<button type="submit" class="btn"></button>
+					<span class="lnr lnr-cross" id="close_search" title="Close Search"></span>
+				</form>
+			</div>
+		</div>
 	</header>
+
+
+</body></html>
